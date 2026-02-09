1871f982d535f28df76716a60c2115c4e318bc83
Willian Oliveira <willian.dev.tech@gmail.com>
2026-02-09 16:05:23 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,26 @@ import { Button } from "./_components/ui/button"
 import { Header } from "./_components/ui/header"
 import { Input } from "./_components/ui/input"
 import { SearchIcon } from "lucide-react"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <div className="p-5">
+      <div className="space-y-6 p-5">
         <h2 className="text-xl font-bold">Olá, Willian!</h2>
-        <p>Segunda-feira, 05 de agosto.</p>
+        <p>Segunda-feira, 09 de Fevereiro.</p>
 
-        <div className="mt-6 flex flex-row items-center gap-2">
+        <div className="flex flex-row items-center gap-2">
           <Input placeholder="Faça sua busca..." />
           <Button size="icon">
             <SearchIcon />
           </Button>
         </div>
 
-        <div className="relative mt-6 h-37.5 w-full rounded-xl">
+        <div className="relative h-37.5 w-full rounded-xl">
           <Image
             alt="Agende nos melhores com Razor Barber"
             src="/banner-01.png"
@@ -28,7 +31,34 @@ export default function Home() {
           />
         </div>
 
-        <div></div>
+        <div className="flex flex-col gap-4">
+          <h2 className="text-xs font-bold text-gray-400 uppercase">
+            Agendemantos
+          </h2>
+          <div>
+            <Card className="p-0">
+              <CardContent className="flex justify-between p-0">
+                <div className="flex flex-col gap-2 p-6">
+                  <Badge className="w-fit">Confirmado</Badge>
+                  <div className="space-y-1.5">
+                    <h3 className="font-semibold">Corte de Cabelo</h3>
+                    <div className="flex items-center gap-2">
+                      <Avatar className="size-10">
+                        <AvatarImage src="https://t3.ftcdn.net/jpg/02/94/29/86/360_F_294298678_i3y46pvuAsGQsfSQw047Yqmvdr74kyfJ.jpg" />
+                      </Avatar>
+                      <p className="text-sm">Barbershop</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col items-center justify-center border-l-2 border-solid p-6">
+                  <p className="text-sm">Fevereiro</p>
+                  <p className="text-2xl">09</p>
+                  <p className="text-sm">09:45</p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
       </div>
     </div>
   )
